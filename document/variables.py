dc07d99b26e93c0d730a16c999ba0e6ce0732df9
luis angel ponce alvarez <poncealvarezluisangel4@gmail.com>
2022-11-04 16:49:43 -0600

    agrego de carpeta de document y proyectos de ellos

---
+++ document/variables.py
@@ -0,0 +1,22 @@
+#Es un espacion en memoria donde se guardan y recuperan los datos que utiliza un programa
+ 
+#Cada variable debe de tener un nombre, con el cual se podrá identificar y referirse a ella, durante el desarrollo de un programa
+
+#En python, el nombre de una variable, no puede coincidir con los nombres de los comandos asignados a este lenguaje de programación, además de que no deberá contener espacios en blanco 
+
+#Es decir, una variable no podría llamarse: "print, nom bre"
+# ya que este es uno de los comandos asignados a python 
+
+#En python, los dos tipos de variables más comunes, son:
+#Las variables que almacenan números
+#Las variables que almacenan texto 
+
+#En las variables que almacenan números hay dos tipos principales: 
+#Los números enteros (Llamados int)
+#Los números decimales o reales (Llamados float)
+
+#Y a las variable que almacenan texto, se le denomina strings(str).
+#Es muy importante, que el contenido a almacenar en este tipo de variables, se encuentre entre comillas. "hola mundo"
+
+#declaración de una variable
+#
