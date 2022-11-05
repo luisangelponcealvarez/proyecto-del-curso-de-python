2992554973f48cf9c3e29f266445226382e2b86f
luis angel ponce alvarez <poncealvarezluisangel4@gmail.com>
2022-11-04 20:12:53 -0600

    agrego de operadores

---
+++ document/Operadores aritméticos.py
@@ -0,0 +1,4 @@
+#-Operadores aritméticos-
+#Son aquellos que "manipulan" datos numéricos, tanto números enteros, así como números decimales también conocidos como reales 
+#suma(+) resta(-) multiplicación(*) exponente(**) módulo(%) división(/) división entera(//)
+
---
+++ document/palabras reservadas.py
@@ -0,0 +1,5 @@
+#-palabras reservadas-
+#Son identificadores para uso exclusivo del lenguaje de programación que no pueden ser utilizadas para identificar y nombrear variables, métodos, objetos o cualquier elemento dentro de nuestro código
+
+#En python, existe un total de 28 palabras reservadas, las cuales fueron asignadas para uso exclusivo del lenguaje de programación: 
+#(and, del, for, is, raise, assert, if, else, elif, from, lambda, return, break, global, not, try, class, except, or, while, continue, exec, import, yield, def, finally, in, print) 
